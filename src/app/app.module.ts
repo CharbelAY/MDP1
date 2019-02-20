d63b0397a90ecc159e9e3affd7bf5eaf09dd5fac
Charbelay <charbel@abouyounes.com>
2019-02-21 00:30:07 +0200

    Working version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AccordionModule } from 'primeng/components/accordion/accordion';
+import { PanelModule } from 'primeng/components/panel/panel';
+import { ButtonModule } from 'primeng/components/button/button';
+import { RadioButtonModule } from 'primeng/components/radioButton/radioButton';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,8 +16,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AccordionModule,
+    PanelModule,
+    ButtonModule,
+    RadioButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
